415a2747aaeda9b24e969c51aefc5c2c6bd9a6c6
coco <meunse1999@gmail.com>
2026-05-31 02:45:22 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/ilson/spotwork/SpotworkApplication.java
@@ -0,0 +1,13 @@
+package com.ilson.spotwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotworkApplication.class, args);
+	}
+
+}
