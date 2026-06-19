9a6763f5d7d873d5619cf21a8a6cc90246fd87d9
coco <meunse1999@gmail.com>
2026-06-19 17:01:44 +0900

    fix: CustomException null validation 추가 및 Validation 에러 응답 개선

--- src/main/java/com/ilson/spotwork/common/exception/CustomException.java
+++ src/main/java/com/ilson/spotwork/common/exception/CustomException.java
@@ -2,13 +2,15 @@ package com.ilson.spotwork.common.exception;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
     public CustomException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
+        super(Objects.requireNonNull(errorCode, "errorCode must not be null").getMessage());
         this.errorCode = errorCode;
     }
 }
