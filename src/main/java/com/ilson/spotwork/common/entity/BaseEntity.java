6acdb19cd0fcbb4a334dfd8c1914c0eaee2b03a8
coco <meunse1999@gmail.com>
2026-06-21 23:21:14 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/com/ilson/spotwork/common/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.ilson.spotwork.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
