6c2cf428a93f2669095d33e668e238d023166a51
coco <meunse1999@gmail.com>
2026-06-23 02:39:03 +0900

    feat: CustomUserDetails 추가

---
+++ src/main/java/com/ilson/spotwork/infra/security/jwt/CustomUserDetails.java
@@ -0,0 +1,45 @@
+package com.ilson.spotwork.infra.security.jwt;
+
+import com.ilson.spotwork.domain.user.entity.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Long userId;
+    private final Role role;
+
+    public CustomUserDetails(Long userId, Role role) {
+        this.userId = userId;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() { return ""; }
+
+    @Override
+    public String getUsername() { return String.valueOf(userId); }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
