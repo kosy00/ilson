07f1a42b7e688e68da15dd4ebb53a4c6aaea1556
coco <meunse1999@gmail.com>
2026-06-24 12:09:25 +0900

    chore: api테스트를 위한 swagger설정 추가

---
+++ src/main/java/com/ilson/spotwork/common/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.ilson.spotwork.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("일손 API")
+                        .description("단기 일자리 O2O 매칭 플랫폼")
+                        .version("v1.0"))
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme));
+    }
+}
