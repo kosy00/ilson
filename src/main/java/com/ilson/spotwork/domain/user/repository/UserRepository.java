b03a64c79bb0e29f9936f7b5ebdc73c07ab9a678
coco <meunse1999@gmail.com>
2026-06-24 12:17:40 +0900

    feat: GenerationType.IDENTITY 수정

--- src/main/java/com/ilson/spotwork/domain/user/repository/UserRepository.java
+++ src/main/java/com/ilson/spotwork/domain/user/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.ilson.spotwork.domain.user.repository;
 import com.ilson.spotwork.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
